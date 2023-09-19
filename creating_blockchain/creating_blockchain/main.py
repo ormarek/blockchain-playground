408b9099c5556354409ca109db69f08491017f5f
ormarek <16357457+ormarek@users.noreply.github.com>
2023-09-19 23:35:29 +0200

    feature: implement simple blockchain class

--- creating_blockchain/creating_blockchain/main.py
+++ creating_blockchain/creating_blockchain/main.py
@@ -1,8 +1,24 @@
 import datetime, hashlib, json
 from flask import Flask, jsonify
 
+class Blockchain:
+    def __init__(self) -> None:
+        self.chain = []
+        self.create_block(proof = 1, prev_hash = None)
+
+    def create_block(self, proof, prev_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'prev_hash': prev_hash}
+        self.chain.append(block)
+        return block
+    
+    def get_previous_block(self):
+        return self.chain(-1)
+
 def main():
-  print('Hello world!')
+    print('Hello world!')
 
 if __name__ == '__main__':
-  main()
+    main()
