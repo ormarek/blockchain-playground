94b85d29b6964844e41d30becbd4aab22cf98c2b
ormarek <16357457+ormarek@users.noreply.github.com>
2023-09-21 22:23:50 +0200

    feature: add web server and requests collection
    Added flask web server to communicate with blockchain and created thunder client collection that holds sample requests

--- creating_blockchain/creating_blockchain/main.py
+++ creating_blockchain/creating_blockchain/main.py
@@ -54,9 +54,42 @@ class Blockchain:
             previous_block = current_block
 
         return True
+    
+app = Flask(__name__)
+blockchain = Blockchain()
+
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    chain = blockchain.chain
+    response = {
+        'chain': chain,
+        'length': len(chain)
+    }
+
+    return jsonify(response), 200
+
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'Valid'}
+    else:
+        response = {'message': 'Invalid'}
+        
+    return jsonify(response), 200
+
+@app.route('/mine_block', methods = ['POST'])
+def mine_block():
+    previous_block = blockchain.get_last_block()
+    proof = blockchain.proof_of_work(previous_block['proof'])
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+
+    return jsonify(block), 200
+
 
 def main():
-    print('Hello world!')
+    app.run(host = '0.0.0.0', port = '5000')
 
 if __name__ == '__main__':
     main()
