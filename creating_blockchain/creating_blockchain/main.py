8df0058cab7e644b9fbc2171694748fe719cdef2
ormarek <16357457+ormarek@users.noreply.github.com>
2023-09-21 14:52:31 +0200

    feature: add chain validation function

--- creating_blockchain/creating_blockchain/main.py
+++ creating_blockchain/creating_blockchain/main.py
@@ -21,14 +21,37 @@ class Blockchain:
         new_proof = 1
         check_proof = False
 
-        while check_proof is False:
+        while check_proof == False:
             hash_operation = hashlib.sha256(str(new_proof**2 - prev_proof**2).encode()).hexdigest()
-            if hash_operation[:4] is '0000':
+            if hash_operation[:4] == '0000':
                 check_proof = True
             else:
                 new_proof += 1
         return new_proof
     
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+    def is_chain_valid(self, chain):
+        prefious_block = chain[0]
+        block_index = 1
+
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(prefious_block):
+                return False
+            
+            prev_proof = prefious_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - prev_proof**2).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+            prev_proof = block
+            block_index += 1
+        return True
+
+
 def main():
     print('Hello world!')
 
